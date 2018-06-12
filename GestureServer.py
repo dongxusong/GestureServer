9560d3958b5ce601c71d6e2dfe307410ff126b0e
DmrfCoder <xuefanggang97@gmail.com>
2018-06-12 23:03:24 +0800

    完成了基本的I、Q数据实时可视化

--- GestureServer.py
+++ GestureServer.py
@@ -1,4 +1,4 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 import random
 
 from flask import Flask
@@ -10,62 +10,82 @@ from matplotlib import animation
 import numpy as np
 import seaborn as sns
 
+
+def Normalize(data):
+    m = np.mean(data)
+    mx = max(data)
+    mn = min(data)
+    return [(float(i) - m) / (mx - mn) for i in data]
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def test():
     return '服务器正常运行'
 
 
-gesture_i=[0]*2200
-gesture_q=[0]*2200
-cur_data_count=0
+gesture_i = [0] * 2200
+gesture_q = [0] * 2200
+cur_data_count = 0
 
-#此方法处理用户注册
-@app.route('/gesture',methods=['POST'])
-def register():
 
+@app.route('/senddata_i', methods=['POST'])
+def getdata_i():
     global gesture_i
-    global gesture_q
 
-    value_i=request.form['gesture_i']
-    value_i=value_i.split(",")
-
-
-    value_q=request.form['gesture_q']
-    value_q=value_q.split(",")
+    value_i = request.form['gesture']
+    value_i = value_i.split(",")
 
     global cur_data_count
+    temp_i = []
 
+    for i in range(0, 110):
+        temp_i.append(float(value_i[i]))
 
+    temp_i = Normalize(temp_i)
 
-    if cur_data_count==4:
-        for i in range(0,550):
+    if cur_data_count == 20:
+        for i in range(0, 110):  # 先这么写吧，优化的时候最好吧这两步放在一个for里面处理
             del gesture_i[0]
-            gesture_i.append(float(value_i[i]))
-
-            del gesture_q[0]
-            gesture_q.append(float(value_q[i]))
-
+            gesture_i.append(float(temp_i[i]))
     else:
         cur_data_count = cur_data_count + 1
-        for i in range(0, 550):
-            gesture_i[cur_data_count*550+i] = float(value_i[i])
-            gesture_q[cur_data_count*550+i] = float(value_q[i])
-
-
-    for i in range(100,200):
-        gesture_i[i] = 1
-
-
-    print("ok")
+        for i in range(0, 110):
+            gesture_i[cur_data_count * 110 + i] = float(temp_i[i])
+    print(temp_i)
     return "ok"
 
 
 
 
 
+@app.route('/senddata_q', methods=['POST'])
+def getdata_q():
+    global gesture_q
 
+    value_i = request.form['gesture']
+    value_i = value_i.split(",")
+
+    global cur_data_count
+    temp_i = []
+
+    for i in range(0, 110):
+        temp_i.append(float(value_i[i]))
+
+    temp_i = Normalize(temp_i)
+
+    if cur_data_count == 20:
+        for i in range(0, 110):  # 先这么写吧，优化的时候最好吧这两步放在一个for里面处理
+            del gesture_q[0]
+            gesture_q.append(float(temp_i[i]))
+    else:
+        cur_data_count = cur_data_count + 1
+        for i in range(0, 110):
+            gesture_q[cur_data_count * 110 + i] = float(temp_i[i])
+    print(temp_i)
+    return "ok"
 
 
 '''
@@ -83,11 +103,10 @@ ax2 = fig.add_subplot(2, 1, 2)
 # 先绘制初始图形，每个子图包含1个正弦波和三个点的散点图
 x = np.arange(0, 2200, 1)  # x轴
 
-
-ax1.set_ylim(-1,1)
+ax1.set_ylim(-1, 1)
 line1, = ax1.plot(x, gesture_i)
 
-ax2.set_ylim(-1,1)
+ax2.set_ylim(-1, 1)
 line2, = ax2.plot(x, gesture_q)
 
 
@@ -122,13 +141,12 @@ def animate(i):
 # blit 选择更新所有点，还是仅更新产生变化的点。应选择True，但mac用户请选择False，否则无法显示动画
 
 
-
 def draw_view():
     ani = animation.FuncAnimation(fig=fig,
                                   func=animate,
                                   frames=100,
                                   init_func=init,
-                                  interval=500,
+                                  interval=100,
                                   blit=False)
     plt.show()
 
@@ -136,11 +154,7 @@ def draw_view():
 def run_server():
     app.run(host='0.0.0.0')
 
+
 if __name__ == '__main__':
-
-   thread.start_new_thread(run_server, ())
-   draw_view()
-
-
-
-
+    thread.start_new_thread(run_server, ())
+    draw_view()
